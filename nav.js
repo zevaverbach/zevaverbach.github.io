eb88de0d6b8e57a243ebbd4a334f3ec2ee3ce827
zevav <zev@averba.ch>
2018-08-01 10:46:45 -0400

    minor aesthetic changes

---
+++ nav.js
@@ -0,0 +1,42 @@
+
+var splitPath = function(path) {
+  var result = path.replace(/\\/g, "/").match(/(.*\/)?(\..*?|.*?)(\.[^.]*?)?(#.*$|\?.*$|$)/);
+  return {
+    dirname: result[1] || "",
+    filename: result[2] || "",
+    extension: result[3] || "",
+    params: result[4] || ""
+  };
+};
+
+d = window.document
+
+var get_current_page_name = function() {
+    page_name = splitPath(d.URL).filename
+    if (!['projects', 'index'].includes(page_name)) {
+		return 'blog' } 
+    if (page_name == 'index') {
+        return 'about'
+    }
+	return page_name
+}
+
+window.onload = function() {
+    current_page_name = get_current_page_name()
+    nav = d.getElementById('nav')
+    Array.prototype.forEach.call(nav.children, function(child, index){
+        if (current_page_name == child.text.toLowerCase()) {   
+            child.className = child.className.concat(' current-page')
+        }
+    })
+    title_element = d.getElementById('title')
+    console.log(current_page_name)
+    if (current_page_name == 'blog') {
+        d.title = d.title.concat(' | Blog')
+        title_element.text = title_element.text.concat("\'s Blog")
+    } else if (current_page_name == 'projects') {
+        d.title = d.title.concat(' | Projects')
+        title_element.text = title_element.text.concat("\'s Projects")
+
+    }
+}
